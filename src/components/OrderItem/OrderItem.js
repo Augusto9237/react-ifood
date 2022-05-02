17eeb847d718ce2d5bb7bfe6628345806cc9f3b1
Augusto9237 <augusto.souza8330@gmail.com>
2022-05-02 16:42:34 -0300

    2 files modified

--- src/components/OrderItem/OrderItem.js
+++ src/components/OrderItem/OrderItem.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 import './styles.css';
-
+import { ImBin } from 'react-icons/im';
 export default function OrderItem({ children }) {
-  return <div className="orderItem-container"></div>;
+  return (
+    <div className="orderItem-container">
+      <div className="orderItem-content">
+        <div className="orderItem-imageProduct">
+          <img src="https://espetinhodesucesso.com.br/wp-content/uploads/2018/03/melhores-carnes-para-churrasco-1200x900.jpg" />
+        </div>
+        <span>Spicy seasoned sea...</span>
+        <span>R$ 50,00</span>
+        <input type="Number" />
+        <span>R$ 100,00</span>
+      </div>
+
+      <div className="orderItem-content">
+        <textarea> </textarea>
+        <button className="orderItem-contentbtnDelete">
+          <ImBin />
+        </button>
+      </div>
+    </div>
+  );
 }
