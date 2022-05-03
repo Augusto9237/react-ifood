e3b1998780e80580c1515851042a825431180277
Augusto9237 <augusto.souza8330@gmail.com>
2022-05-03 15:00:03 -0300

    1 file modified

--- src/components/UI/Header/Header.js
+++ src/components/UI/Header/Header.js
@@ -3,9 +3,15 @@ import React from 'react';
 import './styles.css';
 
 export default function Header({ title, children }) {
+  const data = new Date();
+  const dia = String(data.getDate()).padStart(2, '0');
+  const mes = String(data.getMonth() + 1).padStart(2, '0');
+  const ano = data.getFullYear();
+  const dataAtual = dia + '/' + mes + '/' + ano;
   return (
     <div className="header-container">
       <h1>{title}</h1>
+      <span>{dataAtual}</span>
       <div className="header-menu">{children}</div>
     </div>
   );
