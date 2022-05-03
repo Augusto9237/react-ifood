daf6b1e9e2569df24462da70d5afdf3a6a78fa0e
Augusto9237 <augusto.souza8330@gmail.com>
2022-05-03 15:43:31 -0300

    2 files modified

--- src/components/UI/Header/Header.js
+++ src/components/UI/Header/Header.js
@@ -11,7 +11,7 @@ export default function Header({ title, children }) {
   return (
     <div className="header-container">
       <h1>{title}</h1>
-      <span>{dataAtual}</span>
+      <span className="date">{dataAtual}</span>
       <div className="header-menu">{children}</div>
     </div>
   );
