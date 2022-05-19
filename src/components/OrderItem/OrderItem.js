6371894ae49b9ee087dd5e7678e8635b30c28721
Augusto Sousa <79643279+Augusto9237@users.noreply.github.com>
2022-05-19 18:29:26 -0300

    commands

--- src/components/OrderItem/OrderItem.js
+++ src/components/OrderItem/OrderItem.js
@@ -3,14 +3,12 @@ import React from "react";
 import "./styles.css";
 import { ImBin } from "react-icons/im";
 export default function OrderItem({ children, commands }) {
-  console.log(commands);
   return (
     <div className="orderItem-container">
       <div className="orderItem-content">
         <div className="orderItem-imageProduct">
           <img src="https://espetinhodesucesso.com.br/wp-content/uploads/2018/03/melhores-carnes-para-churrasco-1200x900.jpg" />
         </div>
-
         <span>{commands.items.nameProduct}</span>
         <span>R$ {commands.items.price}</span>
         <input type="Number" value={commands.items.quantity} />
